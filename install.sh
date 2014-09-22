2148cf8e2bdf935188f9f958219e82e09dd622d0
yeongjin jang <blue9057.com>
2014-09-22 03:14:10 -0400

    Added support for bashrc

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ function setup_vim()
 		echo ".vimrc exists, move it to .vimrc.bak"
 		mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
 	fi
-	
+
 	echo 'symlinking .vimrc'
 	ln -s "$PWD/vimrc" "$HOME/.vimrc"
 
@@ -24,16 +24,31 @@ function setup_vim()
 	ln -s "$PWD/vim/autoload/plug.vim" "$HOME/.vim/autoload/plug.vim"
 }
 
+function setup_bash()
+{
+  if [ -e "$HOME/.bashrc" ]; then
+    echo ".bashrc exists, move it to .bashrc.bak"
+    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+  fi
+  echo 'symlinking .bashrc'
+  ln -s "$PWD/bashrc" "$HOME/.bashrc"
+}
+
 
 case "$1" in
-	vim)
-	setup_vim
-	;;
+  vim)
+    setup_vim
+    ;;
 
-	*)
-		echo "usage: $(basename $0) <command>"
-		echo ''
-		echo 'Available commands'
-		echo '	vim	Setup vim and its packages'
-	;;
+  bash)
+    setup_bash
+    ;;
+
+  *)
+    echo "usage: $(basename $0) <command>"
+    echo ''
+    echo 'Available commands'
+    echo '  vim	Setup vim and its packages'
+    echo '  bash  Setup bashrc'
+    ;;
 esac
